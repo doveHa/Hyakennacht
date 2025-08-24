4025de3131348a4684dca7bbe81be4e8c9916c8a
doveHa <hadove02@gmail.com>
2025-08-24 15:37:37 +0900

    JSON 추가

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -1,20 +1,51 @@
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using System.Text.Json;
 
 public class EnemyStats : MonoBehaviour
 {
     public string name;
     private double _health;
     private double _speed;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    void Awake()
     {
-        
+        StreamReader reader =
+            new StreamReader(new FileStream("E:\\Project\\Unity\\Hyakennacht\\Assets\\Resources\\Json\\EnemyStats.json",
+                FileMode.Open));
+        List<EnemyStat> set = JsonSerializer.Deserialize<List<EnemyStat>>(reader.ReadToEnd());
+        Debug.Log(set.Count);
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+    }
+
     void Update()
     {
-        
+    }
+
+    void Hurt(double damage)
+    {
+        if ((_health - damage) <= 0)
+        {
+            Death();
+        }
+        else
+        {
+            _health -= damage;
+        }
+    }
+
+    private void Death()
+    {
+    }
+
+    private class EnemyStat
+    {
+        public string name;
+        public double speed;
+        public double Health;
     }
 }
