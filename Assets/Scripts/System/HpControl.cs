6a6890d4264dca50a3bb4ffb2f8dacbb4bac2cd2
hlj1085 <113406145+hlj1085@users.noreply.github.com>
2025-08-31 04:16:22 +0900

    hpcontrol 수정

--- Assets/Scripts/System/HpControl.cs
+++ Assets/Scripts/System/HpControl.cs
@@ -9,6 +9,12 @@ namespace System
         private int _currentHp;
         private bool _isInvincible;
 
+        //hr
+        public int MaxHp => hpArray.Length;
+        public int CurrentHp => _currentHp;
+        public bool IsFullHp => _currentHp >= MaxHp - 1;
+
+
         void Awake()
         {
             DontDestroyOnLoad(transform.parent.gameObject);
